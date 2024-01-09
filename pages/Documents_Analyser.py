c3a5d548b380f5df51218f35f88409867ddc4d18
dhruvsanan <21f1004102@student.onlinedegree.iitm.ac.in>
2024-01-09 12:29:41 +0530

    Additional Changes

--- pages/Documents_Analyser.py
+++ pages/Documents_Analyser.py
@@ -28,14 +28,14 @@ def get_pf_text(pdf_docs):
     return text
 
 
-def get_text_chunks(raw_text):
-    text_splitter= CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000, 
-        chunk_overlap=200, 
-        length_function = len)
-    text_chunks=text_splitter.split_text(raw_text)
-    return text_chunks
+# def get_text_chunks(raw_text):
+#     text_splitter= CharacterTextSplitter(
+#         separator="\n",
+#         chunk_size=1000, 
+#         chunk_overlap=200, 
+#         length_function = len)
+#     text_chunks=text_splitter.split_text(raw_text)
+#     return text_chunks
 
 
 def text_to_df(raw_text):
