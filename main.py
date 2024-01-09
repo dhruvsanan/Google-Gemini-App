c7a2ac6412c4f6f820530612a20ad9d896cf9df9
dhruvsanan <21f1004102@student.onlinedegree.iitm.ac.in>
2024-01-09 11:39:29 +0530

    Basic Changes

--- main.py
+++ main.py
@@ -2,17 +2,3 @@ import streamlit as st
 
 st.title("Welcome to the Google Gemini App")
 st.header("from the sidebar, choose the app that you want to use")
-# app_selection = st.sidebar.selectbox("Select an app:", ["Conversation App", "Gym Trainer", "Documents Analyser", "Image Recognition"])
-
-# if app_selection == "Conversation App":
-#     st.subheader("Running The App")
-#     import pages.app
-# elif app_selection == "Gym Trainer":
-#     st.subheader("Running The App")
-#     import pages.gym_trainer
-# elif app_selection == "Documents Analyser":
-#     st.subheader("Running The App")
-#     import pages.docs
-# elif app_selection == "Image Recognition":
-#     st.subheader("Running The App")
-#     import pages.vision
