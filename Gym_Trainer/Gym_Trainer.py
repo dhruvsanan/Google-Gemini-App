93b349230b114053f5839143c05376dea4a712fe
dhruv-sanan <dhruvsanan47@gmail.com>
2024-07-30 16:37:16 +0530

    updated model

--- Gym_Trainer/Gym_Trainer.py
+++ Gym_Trainer/Gym_Trainer.py
@@ -8,29 +8,35 @@ from PIL import Image
 
 genai.configure(api_key=os.getenv("GOOGLE_API _KEY"))
 
-image_model=genai.GenerativeModel("gemini-pro-vision")
-text_model=genai.GenerativeModel("gemini-pro")
+image_model = genai.GenerativeModel("gemini-1.5-flash")
+text_model = genai.GenerativeModel("gemini-pro")
 
 
-def gemini_response(input,image,prompt):
-    if input !="":
+def gemini_response(input, image, prompt):
+    if input != "":
         if image:
-            response = image_model.generate_content([input,image,prompt],stream=True, safety_settings={'HARASSMENT':'block_none',})
+            response = image_model.generate_content(
+                [input, image, prompt], stream=True, safety_settings={'HARASSMENT': 'block_none', })
         else:
-            response = image_model.generate_content([input,prompt],stream=True, safety_settings={'HARASSMENT':'block_none'})
+            response = image_model.generate_content(
+                [input, prompt], stream=True, safety_settings={'HARASSMENT': 'block_none'})
 
     else:
-        response = image_model.generate_content([image,prompt],stream=True, safety_settings={'HARASSMENT':'block_none'})
+        response = image_model.generate_content(
+            [image, prompt], stream=True, safety_settings={'HARASSMENT': 'block_none'})
     return response
+
+
 def main():
     load_dotenv()
     st.set_page_config(page_title="Gemini Gym Trainer")
 
     st.header("Gemini Image")
 
-    input = st.text_input("Ask a question ",key="input")
-    uploaded_file = st.file_uploader("Upload the image of your body", type=["jpg", "jpeg", "png","webp"])
-    image=""   
+    input = st.text_input("Ask a question ", key="input")
+    uploaded_file = st.file_uploader("Upload the image of your body", type=[
+                                     "jpg", "jpeg", "png", "webp"])
+    image = ""
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption="Uploaded Image.", use_column_width=True)
@@ -44,14 +50,14 @@ def main():
                     in addition to that provide a complementry gym routine for a week tailored to the input image.
                 """
 
-    submit=st.button("Tell me about my body")
+    submit = st.button("Tell me about my body")
 
-
-    if submit: 
-        response=gemini_response(input,image,input_prompt)
+    if submit:
+        response = gemini_response(input, image, input_prompt)
         st.subheader("The Response is")
         for chunk in response:
             st.write(chunk.text)
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     main()
