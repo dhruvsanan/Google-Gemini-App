c03c2509904c479ef260ce961035985a9ff841f0
dhruv-sanan <dhruvsanan47@gmail.com>
2024-07-30 17:22:48 +0530

    updated model

--- Gym_Trainer/Gym_Trainer.py
+++ Gym_Trainer/Gym_Trainer.py
@@ -43,11 +43,12 @@ def main():
                 You are an expert gymming trainer. Consider yourself as a profesional trainer who provides training plans.
                 """
     input_prompt2 = """
-                You will receive an input image of a body &
+                You will receive an image of a body &
                 you will have to analyse the body type and 
                 state the body type of the person and
                     answer questions based on the input image and
                     in addition to that provide a complementry gym routine for a week tailored to the input image.
+                Make sure you act as a professional trainner. Don't try to give no as answer. Even if you don't understand, just give a generic answer.
                 """
 
     submit = st.button("Tell me about my body")
