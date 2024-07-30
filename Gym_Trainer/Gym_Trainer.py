22477159c967a5be7c23deb8f94cc730de7dae75
dhruv-sanan <dhruvsanan47@gmail.com>
2024-07-30 18:02:38 +0530

    updated model

--- Gym_Trainer/Gym_Trainer.py
+++ Gym_Trainer/Gym_Trainer.py
@@ -11,11 +11,11 @@ genai.configure(api_key=os.getenv("GOOGLE_API _KEY"))
 image_model = genai.GenerativeModel("gemini-1.5-pro")
 
 
-def gemini_response(image, input,  prompt, input_prompt2):
+def gemini_response(image, input,  prompt, input_prompt2, input_prompt3):
     if input:
         if image:
             response = image_model.generate_content(
-                [image, prompt+input_prompt2+" After that, answer the given question", input], stream=True, safety_settings={'HARASSMENT': 'block_none', 'DANGEROUS': 'block_none'})
+                [image, prompt+input_prompt3, input], stream=True, safety_settings={'HARASSMENT': 'block_none', 'DANGEROUS': 'block_none'})
         else:
             response = image_model.generate_content(
                 [input, prompt], stream=True, safety_settings={'HARASSMENT': 'block_none', 'DANGEROUS': 'block_none'})
@@ -29,7 +29,7 @@ def main():
     load_dotenv()
     st.set_page_config(page_title="Gemini Gym Trainer")
 
-    st.header("Gemini Image")
+    st.header("Gemini Gym Trainer")
 
     input = st.text_input("Ask a question ", key="input")
     uploaded_file = st.file_uploader("Upload the image of your body", type=[
@@ -48,11 +48,18 @@ def main():
                     After indentifying the body type, state the body type and provide a complementary gym routine for a week tailored to the input image.
                 Make sure you act as a professional trainner. Don't try to give no as answer. Even if you don't understand, just give a generic answer.
                 """
+    input_prompt3 = """
+                You will receive an image of a body &
+                your job is to give a rough estimate of what the body type might be.
+                    After that, answer the given question based on the type of body.
+                Make sure you act as a professional trainner. Don't try to give no as answer. Even if you don't understand, just give a generic answer.
+                """
 
     submit = st.button("Tell me about my body")
 
     if submit:
-        response = gemini_response(image, input, input_prompt, input_prompt2)
+        response = gemini_response(
+            image, input, input_prompt, input_prompt2, input_prompt3)
         st.subheader("The Response is")
         for chunk in response:
             st.write(chunk.text)
